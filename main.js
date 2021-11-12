8c9eaebcb51e84f895973478f5fd71ca0f769d1e
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-11-12 18:29:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 function preload()
 {
     img = loadImage("dog_cat.jpg");
@@ -17,23 +18,22 @@ function setup()
 function draw()
 {
 image(img, 0, 0, 640, 420);
-fill("#FF0000");
-text("Dog", 45, 75);
-noFill();
-stroke("#FF0000");
-rect(30, 50, 450, 350);
 
-fill("#FF0000");
-text("Cat", 320, 120);
-noFill();
-stroke("#FF0000");
-rect(300, 90, 270, 320);
 
-fill("#FF0000");
-text("bowl", 270, 270);
-noFill();
-stroke("#FF0000");
-rect(260, 250, 200, 165);
+    if(status != "")
+    {
+        for(i = 0; i< objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: objects detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function modelLoaded()
@@ -50,4 +50,6 @@ function gotResult(error, results)
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
+
